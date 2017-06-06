b3634ea97886eb3e0db6b1431cab78e825bcf9a2
mbeyens <matthias.beyens@uantwerpen.be>
2017-06-06 15:20:37 +0200

    improved germline filter

--- pyampli/germline_filter.py
+++ pyampli/germline_filter.py
@@ -22,16 +22,4 @@ def add_variant_info_fields(number_after_amplicons_all, new_variant_info_field, 
     variant.add_info('AmpCR', number_after_amplicons_all['ref_amps']['bam_normal'])
     variant.add_info('AmpC', number_after_amplicons_all['total_amps']['bam_normal'])
 
-    if 'AD' in variant.FORMAT and len(variant.alleles) == 2:
-        # some stats.
-        offset[var_type]['ref'].append(abs(refs - variant.samples[0]['AD'][0]))
-        dp[var_type]['ref'].append(variant.samples[0]['AD'][0])
-        offset[var_type]['alt'].append(abs(variant.samples[0]['AD'][1]))
-        dp[var_type]['alt'].append(variant.samples[0]['AD'][1])
-        # add to variant record.
-        AmpF_O_R = refs - variant.samples[0]['AD'][0]
-        AmpF_O_A = alts - variant.samples[0]['AD'][1]
-        variant.add_info('AmpF_OR', AmpF_O_R)
-        variant.add_info('AmpF_OA', AmpF_O_A)
-
     return variant
