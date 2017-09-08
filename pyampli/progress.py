8b42a86f7e714ca7ef024ad6e1f9789a84fe0bf4
mbeyens <matthias.beyens@uantwerpen.be>
2017-09-08 13:56:30 +0200

    progress correction

--- pyampli/progress.py
+++ pyampli/progress.py
@@ -17,13 +17,13 @@ def update_progress(variant_number, amount_total_variants):
         time.sleep(3)
         block = int(round(barLength * 1))
         text = "\r\>>> Status : [{0}] {1}% {2} <<<".format("#" * block + "-" * (barLength - block),
-                                                                               round((1 * 100), 2), "finished")
+                                                                               round((1 * 100), 2), "finished\n")
         sys.stdout.write(text)
         sys.stdout.flush()
         logging.info('VCF processing done')
     else:
         block = int(round(barLength * progress))
-        text = "\r\>>> Status : [{0}] {1}% {2} <<<".format("#" * block + "-" * (barLength - block),
+        text = "\r>>> Status : [{0}] {1}% {2} <<<".format("#" * block + "-" * (barLength - block),
                                                                                round((progress * 100), 2), status)
         sys.stdout.write(text)
         sys.stdout.flush()
