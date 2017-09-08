a1268fcbfb0a966675a26379b48f2069a9f960e0
mbeyens <matthias.beyens@uantwerpen.be>
2017-09-08 11:42:18 +0200

    error logging

--- pyampli/pyAmpli.py
+++ pyampli/pyAmpli.py
@@ -105,7 +105,7 @@ def main():
 
     else:
         logging.error(file_not_found)
-        logging.error('Please fix file location or file name, and before restarting pyAmpli')
+        logging.error('Please fix file location or file name, before restarting pyAmpli')
 
 
 if __name__ == "__main__":
