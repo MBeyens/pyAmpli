7c29cea7cf5b8a5ec45c2f30d72ccd4803670098
mbeyens <matthias.beyens@uantwerpen.be>
2017-09-08 14:24:58 +0200

    config load function

--- pyampli/config.py
+++ pyampli/config.py
@@ -4,13 +4,17 @@ import os, sys, logging, yaml
 
 
 def config_load(config_filename):
-    
-    try:
+
+    if os.path.isfile(infile):
         config_filename = os.path.realpath(config_filename)
-        with open(config_filename, 'r') as yml_file:
-            cfg = yaml.load(yml_file)
-            logging.info('Using config file: %s', config_filename)
-        return cfg
-    except IOError:
-        logging.error('Cannot find/load config file (%s)', config_filename, exc_info=True)
+        try:
+            with open(config_filename, 'r') as yml_file:
+                cfg = yaml.load(yml_file)
+                logging.info('Using configuration file: %s', config_filename)
+            return cfg
+        except:
+            logging.error('Cannot load configuration file (%s), although it exists', config_filename, exc_info=True)
+            sys.exit(0)
+    else:
+        logging.error('Cannot find configuration file (%s)', config_filename, exc_info=True)
         sys.exit(0)
