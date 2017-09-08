5d4512773f9effe5783c2eb9ecff4d9cf2e68477
mbeyens <matthias.beyens@uantwerpen.be>
2017-09-08 14:05:41 +0200

    progress correction

--- pyampli/progress.py
+++ pyampli/progress.py
@@ -6,7 +6,8 @@ import sys, logging, time
 def update_progress(variant_number, amount_total_variants):
     progress = (float(variant_number) / float(amount_total_variants))
     barLength = 100  # Modify this to change the length of the progress bar
-    status = "- running - "
+    status = "- running ({0}/{1})- ".format(variant_number, amount_total_variants)
+    status = rolling_ball()
     if isinstance(progress, int):
         progress = float(progress)
     if progress < 0:
@@ -16,14 +17,14 @@ def update_progress(variant_number, amount_total_variants):
 
         time.sleep(3)
         block = int(round(barLength * 1))
-        text = "\r\>>> Status : [{0}] {1}% {2} <<<".format("#" * block + "-" * (barLength - block),
-                                                                               round((1 * 100), 2), "finished\n")
+        text = "\r --> Status : [{0}] {1}% {2} \n".format("#" * block + "-" * (barLength - block),
+                                                                               round((1 * 100), 2), "finished")
         sys.stdout.write(text)
         sys.stdout.flush()
         logging.info('VCF processing done')
     else:
         block = int(round(barLength * progress))
-        text = "\r>>> Status : [{0}] {1}% {2} <<<".format("#" * block + "-" * (barLength - block),
+        text = "\r --> Status : [{0}] {1}% {2} ".format("#" * block + "-" * (barLength - block),
                                                                                round((progress * 100), 2), status)
         sys.stdout.write(text)
         sys.stdout.flush()
