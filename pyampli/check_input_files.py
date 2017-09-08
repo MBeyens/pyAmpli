744cf93f6dad86a957e2f323fac46e593a16504b
mbeyens <matthias.beyens@uantwerpen.be>
2017-09-08 11:35:51 +0200

    check for correct extension

--- pyampli/check_input_files.py
+++ pyampli/check_input_files.py
@@ -14,7 +14,8 @@ def check_extension(file_type, infile):
             decomp_file_name = decompress_file(infile)
             logging.info('Decompressed file %s', decomp_file_name)
         else:
-            logging.error('File extension (%s) is not correct or file compression is not supported (%s)', file_type, infile)
+            logging.error('File extension of your reference genome is not correct or file compression is not supported (%s) !! should be .fa, .fasta !!', infile)
+            logging.error('Please fix latter first, before restarting pyAmpli!')
             sys.exit(0)
     else:
         if infile.endswith(str(file_type)):
@@ -24,6 +25,7 @@ def check_extension(file_type, infile):
             logging.info('Decompressed file %s', decomp_file_name)
         else:
             logging.error('File extension (%s) is not correct or file compression is not supported (%s)', file_type, infile)
+            logging.error('Please fix latter error first, before restarting pyAmpli!')
             sys.exit(0)
     return decomp_file_name
 
